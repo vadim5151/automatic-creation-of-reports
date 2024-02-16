24e5c0deac5964be5cf997b4d055020d099d72d6
unknown <ocirovvadim51@gmail.com>
2024-02-16 19:28:08 +1200

    added img procesin

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 import re
+import os 
 from copy import copy
 from bs4 import BeautifulSoup
 from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+from PIL import Image
+import shutil
 
 
 def get_url_user_and_check_it():
@@ -29,17 +32,31 @@ def setting_styles(doc):
     style.font.name =  "Times New Roman"
     return style
 
-def fill_in_file(all_tags,doc):      
-    for i in all_tags: 
-        if '<p' in i:
-            p = doc.add_paragraph(BeautifulSoup(i,features="html.parser").text)
+def fill_in_file(all_tags,doc):  
+    count = 0    
+    for tag in all_tags: 
+        real_tag = BeautifulSoup(tag,features="html.parser")
+        if  'mw-file-description' in tag:
+            count += 1
+            img_link = real_tag.find('img').get('src')
+            response = requests.get(f'https:{img_link}')
+            response.raise_for_status()
+            p = doc.add_paragraph()
+            with open(f'img/picture{count}.jpg', "wb") as file:
+                file.write(response.content)
+            run = p.add_run()
+            run.add_picture(image_to_jpg(f'img/picture{count}.jpg'))
+            p.alignment =  WD_ALIGN_PARAGRAPH.CENTER
+            delete_everything_in_folder('img')
+        if '<p>' in tag:
+            p = doc.add_paragraph(real_tag.text)
             p.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
             continue
         for a in range(1,5):
-            if f'<h{a}' in i:
-                head = doc.add_heading(BeautifulSoup(i,features="html.parser").text, level=a)
+            if f'<h{a}' in tag:
+                head = doc.add_heading(real_tag.text, level=a)
                 head.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            
+                
 def get_rid_of_unnecessary_sections_bottom(all_tags):   
     reversed_all_tags = list(reversed(all_tags))
     for i in reversed_all_tags:
@@ -53,11 +70,11 @@ def get_rid_of_unnecessary_sections_from_above(all_tags):
     for i in copy_all_tags:
         if'<p>' in str(i):
             index = copy_all_tags.index(i)
-            del copy_all_tags[0:index-2]
+            del copy_all_tags[0:index-3]
             return copy_all_tags
 
 def get_tags_without_notes(all_tags):
-        return [re.sub(r"\[.*?\]",r"", str(i)) for i in all_tags]
+        return [re.sub(r"\[.*?\]",r"", str(tag)) for tag in all_tags]
 
 def try_save(doc,file_name):
         try:
@@ -65,18 +82,31 @@ def try_save(doc,file_name):
         except PermissionError:
             print('Не удалось сохранить файл ')
 
+def image_to_jpg(image_path):
+    Image.open(image_path).convert('RGB').save(image_path)
+    return image_path
+
+def delete_everything_in_folder(folder_path):
+                shutil.rmtree(folder_path)
+                os.mkdir(folder_path)
+
 if __name__ == '__main__':
     response = get_url_user_and_check_it()
 
     soup = BeautifulSoup(response.text, features="html.parser")
     mw_content_text_block = soup.find('div',id = 'mw-content-text')
-
+    
+    
     doc = Document()
     
-    all_tags = soup.find_all(['p', 'h1', 'h2', 'h3', 'h4'])
+    main_block = soup.find('div',class_="mw-content-ltr mw-parser-output")
+    
+    
+    all_tags = main_block.find_all(['p', 'h1', 'h2', 'h3', 'h4','a'])
+
     styles = setting_styles(doc)
     all_tags = get_rid_of_unnecessary_sections_bottom(all_tags)
-    all_tags = get_rid_of_unnecessary_sections_from_above(all_tags)
+    # all_tags = get_rid_of_unnecessary_sections_from_above(all_tags)
     all_tags = get_tags_without_notes(all_tags)
     fill_in_file(all_tags,doc)
     file_name = input('Выберите имя файла: ')
--- test.py
+++ test.py
@@ -1,9 +1,21 @@
+from docx import Document
+import requests
 import re
+from copy import copy
+from bs4 import BeautifulSoup
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Mm
+from pathlib import Path
+from PIL import Image
+import io
 
-   
-all_tags = (['-privet','[privet s kvadrat skobkami]','-poka'])
-
-def get_tags_without_notes(all_tags):
-    return [re.sub(r"\[.*?\]",r"", str(i)) for i in all_tags]
-
-print(*get_tags_without_notes(all_tags),sep='\n')
+def image_to_jpg(image_path):
+    Image.open(image_path).convert('RGB').save(image_path)
+    return image_path
+    
+ 
+doc = Document()
+doc.add_picture(image_to_jpg('picture2.jpg')   )
+doc.save('dfghjk.docx')
